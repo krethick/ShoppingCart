54075fd5afbb0cebc30b47e7ae10061b97680535
Krethick Chakravarthy <krethick.chakravarthy@gavait.com>
2023-05-08 15:30:43 +0530

    Axios used for redux api calls

--- src/containers/ProductListing.tsx
+++ src/containers/ProductListing.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 import { useSelector } from "react-redux";
 import  ProductComponent from "./ProductComponent";
 
 const ProductListing: React.FC = () => {
      const products = useSelector((state)=> state);
-     console.log(products);
+
+     const fetchProducts = async () => {
+       const response = await axios
+           .get("https://fakestoreapi.com/products")
+           .catch((err) => {
+              console.log("Err",err);
+             });
+             console.log(response);
+           };
+     useEffect(() => {
+        fetchProducts();
+     }, []);
+    console.log(products);
   return (
     <div className="ui grid container">
        <ProductComponent />
