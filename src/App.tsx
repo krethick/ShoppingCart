288c9bd80eadb77229bf6daf07703c1762495b4a
Krethick Chakravarthy <krethick.chakravarthy@gavait.com>
2023-05-08 11:26:57 +0530

    added react components as Header,ProductComponents,ProductDetail,ProductListing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-
+import Header from "./containers/Header"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
-      <h1>Hello</h1>
+       <Header />
     </div>
   );
 }
---
+++ src/containers/ProductListing.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProductListing: React.FC = () => {
+  return (
+    <div>
+      <h1>ProductListing</h1>
+    </div>
+  );
+};
+
+export default ProductListing;
