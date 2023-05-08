502bf4924f3cd75419b6914873c181a43d51ff70
Krethick Chakravarthy <krethick.chakravarthy@gavait.com>
2023-05-08 12:02:45 +0530

    Added Routing to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import Header from "./containers/Header"
+import React, { FC } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from "./containers/Header";
+import ProductListing from "./containers/ProductListing";
+import ProductDetail from "./containers/ProductDetail";
 import './App.css';
 
-function App() {
+const App: FC = () => {
   return (
     <div className="App">
-       <Header />
+       <Router>
+           <Header />
+           <Routes>
+            <Route path="/" element={<ProductListing />} />
+            <Route path="/product/:productId" element={<ProductDetail />} />
+            <Route> 404 Not Found!</Route>
+           </Routes>
+       </Router>
     </div>
   );
 }
