9a8add2e40baab2ac9c8b11576a3fe05ad0bd8c9
Krethick Chakravarthy <krethick.chakravarthy@gavait.com>
2023-05-08 13:19:20 +0530

    useselector to access state

--- src/containers/ProductComponent.tsx
+++ src/containers/ProductComponent.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const ProductComponent: React.FC = () => {
+      const products = useSelector((state:any)=> state.productState.products);
+      const {id, title} = products[0];
   return (
-    <div>
-      <h1>ProductComponent</h1>
+
+    <div style={{ margin: "20px" }}>
+     <p></p>
+     <p></p>
+     <p></p>
+     <div className="ui link cards" style={{ width: "500px", height: "300px" }}>
+       <div className="card">
+         <div className="image"></div>
+         <div className="content">
+            <div className="header">{title}</div>
+          </div>
+         </div>
+        </div>
     </div>
   );
 };
--- src/containers/ProductListing.tsx
+++ src/containers/ProductListing.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import  ProductComponent from "./ProductComponent";
 
 const ProductListing: React.FC = () => {
+     const products = useSelector((state)=> state);
+     console.log(products);
   return (
-    <div>
-      <h1>ProductListing</h1>
+    <div className="ui grid container">
+       <ProductComponent />
+      <h1 style={{ margin: "40px", textAlign: "right" }}>Product Listing</h1>
     </div>
   );
 };
