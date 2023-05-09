2e3bef9696a4ab40acfdaf439489c200dfd83ab3
Krethick Chakravarthy <krethick.chakravarthy@gavait.com>
2023-05-10 00:04:27 +0530

    Render products listing pages

--- src/containers/ProductDetail.tsx
+++ src/containers/ProductDetail.tsx
@@ -6,6 +6,7 @@ import { selectedProducts } from "./redux/actions/productsActions";
 
 const ProductDetail: React.FC = () => {
   const product = useSelector((state:any) => state.product);
+  const {image,title,price,category,description} = product
   const { productId } = useParams();
   const dispatch = useDispatch();
   console.log(product);
@@ -25,11 +26,39 @@ const ProductDetail: React.FC = () => {
     if (productId && productId !== "") fetchProductDetail();
   }, [productId]);
 
-  return (
-    <div>
-      <h1>ProductDetail</h1>
+ return (
+    <div className="ui grid container">
+      {Object.keys(product).length === 0 ? (
+        <div>...Loading</div>
+      ) : (
+        <div className="ui placeholder segment">
+          <div className="ui two column stackable center aligned grid">
+            <div className="ui vertical divider">AND</div>
+            <div className="middle aligned row">
+              <div className="column lp">
+                <img className="ui fluid image" src={image} />
+              </div>
+              <div className="column rp">
+                <h1>{title}</h1>
+                <h2>
+                  <a className="ui teal tag label">${price}</a>
+                </h2>
+                <h3 className="ui brown block header">{category}</h3>
+                <p>{description}</p>
+                <div className="ui vertical animated button" tabIndex={0}>
+                  <div className="hidden content">
+                    <i className="shop icon"></i>
+                  </div>
+                  <div className="visible content">Add to Cart</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
+
 
 export default ProductDetail;
